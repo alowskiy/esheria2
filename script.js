ddecac2ed6716a551aaa1750e650500a12118a6a
alowskiy <106418813+alowskiy@users.noreply.github.com>
2024-04-07 14:05:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleFunc(e) {
+  e.classList.toggle("change");
+  document.getElementById("btn-toggle").classList.toggle("show");
+}
---
+++ script2.js
@@ -0,0 +1,5 @@
+function toggleFunc(e) {
+  e.classList.toggle("change");
+  document.getElementById("btn-toggle").classList.toggle("show");
+}
+
